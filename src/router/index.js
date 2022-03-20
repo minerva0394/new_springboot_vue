b77134ed9d5dc0f150f2f6f0a7832aa29d7ac27d
曾智麟 <>
2022-03-21 03:10:01 +0800

    Revert "2022-03-21 03:06"
    This reverts commit fc9d071c46d61ca47c2e24dbd3e705f0f1440ac5.

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import store from "@/store";
+import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    component: () => import('../views/Manage.vue'),
-    redirect: "/home",
-    children: [
-      { path: 'home', name: '首页', component: () => import('../views/Home.vue')},
-      { path: 'user', name: '用户管理', component: () => import('../components/User')},
-    ]
+    name: 'home',
+    component: HomeView
   },
   {
     path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  },
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: process.env. BASE_URL,
   routes
-})
-
-// 路由守卫
-router.beforeEach((to, from, next) => {
-  localStorage.setItem("currentPathName", to.name)  // 设置当前的路由名称，为了在Header组件中去使用
-  store.commit("setPath")  // 触发store的数据更新
-  next()  // 放行路由
 })
 
 export default router
