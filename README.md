a8c2a029e7b7e955e3fe833bb143e9d6c7b0b3a8
曾智麟 <>
2022-03-21 02:41:28 +0800

    test

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 #编辑记录
 ## update 2022-03-20
+<<<<<<< HEAD
 2022-03-20 14:10
 基于mybatis-plus 和 swaggerUI实现后台分页功能<br>
 tolist：swaggerUI还需要后续完善
 ## update 2022-03-20
+=======
+>>>>>>> parent of 2a9ff1a (2022-03-20 19:54)
 2022-03-20 01:09
 实现后端分页查询接口
 ## update 2022-03-19
--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import './assets/gloable.css'
-import request from "@/utils/request";
+import './assets/gloable.css';
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI, { size: "mini" });
-
-Vue.prototype.request=request
+Vue.use(ElementUI, {size: "small"});
 
 new Vue({
     router,
