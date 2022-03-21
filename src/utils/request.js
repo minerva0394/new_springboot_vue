3ad759ee0480b4144e383bed0de3804a6b5391de
曾智麟 <>
2022-03-21 10:14:00 +0800

    Revert "2022-03-20 19:54"
    This reverts commit 2a9ff1a165b0cc20caf10b8f7b68acad59dfae98.

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
+import router from "@/router";
+import {serverIp} from "../../public/config";
 
 const request = axios.create({
-    baseURL: 'http://127.0.0.1:9091',
-    timeout: 5000
+    baseURL: `http://127.0.0.1:9090`,
+    timeout: 30000
 })
 
 // request 拦截器
@@ -10,8 +12,11 @@ const request = axios.create({
 // 比如统一加token，对请求参数统一加密
 request.interceptors.request.use(config => {
     config.headers['Content-Type'] = 'application/json;charset=utf-8';
+    let user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null
+    if (user) {
+        config.headers['token'] = user.token;  // 设置请求头
+    }
 
-    // config.headers['token'] = user.token;  // 设置请求头
     return config
 }, error => {
     return Promise.reject(error)
@@ -30,6 +35,14 @@ request.interceptors.response.use(
         if (typeof res === 'string') {
             res = res ? JSON.parse(res) : res
         }
+        // 当权限验证不通过的时候给出提示
+        if (res.code === '401') {
+            // ElementUI.Message({
+            //     message: res.msg,
+            //     type: 'error'
+            // });
+            router.push("/login")
+        }
         return res;
     },
     error => {
@@ -40,3 +53,4 @@ request.interceptors.response.use(
 
 
 export default request
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -56,7 +56,7 @@
         </div>
         <el-dropdown style="width: 70px; cursor: pointer">
           <span>王小虎</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
-          <el-dropdown-menu slot="dropdown">
+          <el-dropdown-menu slot="dropdown" >
             <el-dropdown-item style="font-size: 14px; padding: 5px 0">个人信息</el-dropdown-item>
             <el-dropdown-item style="font-size: 14px; padding: 5px 0">退出</el-dropdown-item>
           </el-dropdown-menu>
@@ -73,56 +73,30 @@
         </div>
 
         <div style="margin: 10px 0">
-          <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search" v-model="username"
-                    clearable></el-input>
-          <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message" class="ml-5"
-                    v-model="email" clearable></el-input>
-          <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5"
-                    v-model="address" clearable></el-input>
+          <el-input style="width: 200px" placeholder="请输入名称" suffix-icon="el-icon-search" v-model="username"></el-input>
+          <el-input style="width: 200px" placeholder="请输入邮箱" suffix-icon="el-icon-message" class="ml-5"></el-input>
+          <el-input style="width: 200px" placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5"></el-input>
           <el-button class="ml-5" type="primary" @click="load">搜索</el-button>
-          <el-button type="warning" @click="reset">重置</el-button>
         </div>
 
         <div style="margin: 10px 0">
-          <el-button type="primary" @click="handleAdd">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
-          <el-popconfirm
-              class="ml-5"
-              confirm-button-text='确定'
-              cancel-button-text='我再想想'
-              icon="el-icon-info"
-              icon-color="red"
-              title="您确定批量删除这些数据吗？"
-              @confirm="delBatch"
-          >
-            <el-button type="danger" slot="reference">批量删除 <i class="el-icon-remove-outline"></i></el-button>
-          </el-popconfirm>
-          <el-button type="primary" class="ml-5">导入 <i class="el-icon-bottom"></i></el-button>
+          <el-button type="primary">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+          <el-button type="danger">批量删除 <i class="el-icon-remove-outline"></i></el-button>
+          <el-button type="primary">导入 <i class="el-icon-bottom"></i></el-button>
           <el-button type="primary">导出 <i class="el-icon-top"></i></el-button>
         </div>
 
-        <el-table :data="tableData" border stripe :header-cell-class-name="headerBg"
-                  @selection-change="handleSelectionChange">
-          <el-table-column type="selection" width="55"></el-table-column>
+        <el-table :data="tableData" border stripe :header-cell-class-name="headerBg">
           <el-table-column prop="id" label="ID" width="80"></el-table-column>
           <el-table-column prop="username" label="用户名" width="140"></el-table-column>
           <el-table-column prop="nickname" label="昵称" width="120"></el-table-column>
           <el-table-column prop="email" label="邮箱"></el-table-column>
           <el-table-column prop="phone" label="电话"></el-table-column>
           <el-table-column prop="address" label="地址"></el-table-column>
-          <el-table-column label="操作" width="200" align="center">
+          <el-table-column label="操作"  width="200" align="center">
             <template slot-scope="scope">
-              <el-button type="success" @click="handleEdit(scope.row)">编辑 <i class="el-icon-edit"></i></el-button>
-              <el-popconfirm
-                  class="ml-5"
-                  confirm-button-text='确定'
-                  cancel-button-text='我再想想'
-                  icon="el-icon-info"
-                  icon-color="red"
-                  title="您确定删除吗？"
-                  @confirm="del(scope.row.id)"
-              >
-                <el-button type="danger" slot="reference">删除 <i class="el-icon-remove-outline"></i></el-button>
-              </el-popconfirm>
+              <el-button type="success">编辑 <i class="el-icon-edit"></i></el-button>
+              <el-button type="danger">删除 <i class="el-icon-remove-outline"></i></el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -137,32 +111,6 @@
               :total="total">
           </el-pagination>
         </div>
-
-        <el-dialog title="用户信息" :visible.sync="dialogFormVisible" width="30%">
-          <el-form label-width="80px" size="small">
-            <el-form-item label="用户名">
-              <el-input v-model="form.username" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="昵称">
-              <el-input v-model="form.nickname" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="邮箱">
-              <el-input v-model="form.email" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="电话">
-              <el-input v-model="form.phone" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="地址">
-              <el-input v-model="form.address" autocomplete="off"></el-input>
-            </el-form-item>
-          </el-form>
-          <div slot="footer" class="dialog-footer">
-            <el-button @click="dialogFormVisible = false">取 消</el-button>
-            <el-button type="primary" @click="save">确 定</el-button>
-          </div>
-        </el-dialog>
-
-
       </el-main>
 
     </el-container>
@@ -170,8 +118,6 @@
 </template>
 
 <script>
-
-import request from "@/utils/request";
 
 export default {
   name: 'Home',
@@ -182,12 +128,7 @@ export default {
       pageNum: 1,
       pageSize: 5,
       username: "",
-      email: "",
-      address: "",
-      form: {},
-      dialogFormVisible: false,
-      multipleSelection: [],
-      msg: "test",
+      msg: "hello 青哥哥",
       collapseBtnClass: 'el-icon-s-fold',
       isCollapse: false,
       sideWidth: 200,
@@ -213,71 +154,12 @@ export default {
       }
     },
     load() {
-      request.get("/user/page", {
-        params: {
-          pageNum: this.pageNum,
-          pageSize: this.pageSize,
-          username: this.username,
-          email: this.email,
-          address: this.address,
-        }
-      }).then(res => {
+      fetch("http://localhost:9091/user/page?pageNum="+this.pageNum+"&pageSize=" + this.pageSize + "&username=" + this.username)
+          .then(res => res.json()).then(res => {
         console.log(res)
-
-        this.tableData = res.records
+        this.tableData = res.data
         this.total = res.total
-
       })
-    },
-    save() {
-      request.post("/user", this.form).then(res => {
-        if (res) {
-          this.$message.success("保存成功")
-          this.dialogFormVisible = false
-          this.load()
-        } else {
-          this.$message.error("保存失败")
-        }
-      })
-    },
-    handleAdd() {
-      this.dialogFormVisible = true
-      this.form = {}
-    },
-    handleEdit(row) {
-      this.form = row
-      this.dialogFormVisible = true
-    },
-    del(id) {
-      request.delete("/user/" + id).then(res => {
-        if (res) {
-          this.$message.success("删除成功")
-          this.load()
-        } else {
-          this.$message.error("删除失败")
-        }
-      })
-    },
-    handleSelectionChange(val) {
-      console.log(val)
-      this.multipleSelection = val
-    },
-    delBatch() {
-      let ids = this.multipleSelection.map(v => v.id)  // [{}, {}, {}] => [1,2,3]
-      request.post("/user/del/batch", ids).then(res => {
-        if (res) {
-          this.$message.success("批量删除成功")
-          this.load()
-        } else {
-          this.$message.error("批量删除失败")
-        }
-      })
-    },
-    reset() {
-      this.username = ""
-      this.email = ""
-      this.address = ""
-      this.load()
     },
     handleSizeChange(pageSize) {
       console.log(pageSize)
@@ -295,6 +177,6 @@ export default {
 
 <style>
 .headerBg {
-  background: #eee !important;
+  background: #eee!important;
 }
 </style>
